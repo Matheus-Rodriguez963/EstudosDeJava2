9ee03c7b413bda255fa393403fd278f060257ffb
Matheus Silva <massilva.963@gmail.com>
2025-07-25 10:12:02 -0300

    Aula de Scanner

---
+++ src/Condicoes/ExameChunin.java
@@ -0,0 +1,18 @@
+package Condicoes;
+
+// Exame Chunin: Verificando se o ninja tem idade suficiente para competir
+import java.util.Scanner;
+public class ExameChunin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a sua idade, jovem ninja: ");
+        int idade = scanner.nextInt();
+        if (idade >= 12) {
+            System.out.println("Parabéns! Você pode participar do Exame Chunin!");
+        } else {
+            System.out.println("Você ainda é muito jovem para o Exame Chunin. Continue treinando!");
+        }
+        scanner.close();
+    }
+}
+
--- src/Condicoes/ScannerDoUsuario.java
+++ src/Condicoes/ScannerDoUsuario.java
@@ -11,7 +11,34 @@ public class ScannerDoUsuario {
          * Objetivo: O usuario vai criar o ninja e vamos valor os dados
          * */
 
+
+        //Abrir o Scanner
         Scanner caixaDeTexto = new Scanner(System.in);
+
+
+        // Recceber nome do ninja
+        System.out.println("Escreva aqui o nome do ninja: ");
+        String nomeDoNinja = caixaDeTexto.nextLine();
+        System.out.println("O nome do ninja e: ");
+        System.out.println(nomeDoNinja);
+
+
+        //Recebe Idade do ninja
+        System.out.println("Escreva aqui a idade do seu ninja: ");
+        int idadeDoNinja = caixaDeTexto.nextInt();
+        System.out.println("A ideade do ninja e: " + idadeDoNinja + " anos");
+
+        // tratamento de dados
+        if (idadeDoNinja >=18 ){
+            System.out.println("Esse ninja pode ir para missao fora da aldeia, pois e maior de idade");
+
+        } else {
+            System.out.println("Esse ninja e muito novo para missoes como essa, precisara de mais treinamento");
+        }
+
+
+        // Fechar sempre o Scanner
+        caixaDeTexto.close();
     }
 
 
